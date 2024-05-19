4a651f97992100b4db776cb9406ca095a69c4cb9
Sarvesh Sataye <sarvesh.sataye@neosoftmail.com>
2024-05-19 22:43:54 +0530

    new headnumber added

--- excels/views.py
+++ excels/views.py
@@ -12,8 +12,13 @@ def upload_excel(request):
     if request.method == 'POST' and request.FILES['excel_file']:
         excel_file = request.FILES['excel_file']
 
+
+        header_row_num = int(request.POST.get('header_row_num', 0))  # Get the starting row from the request, default is 0
+        if(header_row_num > 0):
+            header_row_num = header_row_num -1
+
         # Read the Excel file using pandas
-        df = pd.read_excel(excel_file)
+        df = pd.read_excel(excel_file,skiprows=header_row_num)
         
         # Get column headers
         column_names = df.columns.tolist()
@@ -28,7 +33,11 @@ def create_excel(request):
         excel_file = request.FILES['excel_file']
         column_names = request.POST.getlist('selected_columns')
 
-        df = pd.read_excel(excel_file)
+        header_row_num = int(request.POST.get('header_row_num', 0))  # Get the starting row from the request, default is 0
+        if(header_row_num > 0):
+            header_row_num = header_row_num -1
+            
+        df = pd.read_excel(excel_file,skiprows=header_row_num)
 
         # Filter columns
         filtered_df = df[column_names]
